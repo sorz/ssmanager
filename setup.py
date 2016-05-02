1b7da5179dcf4af6b2db2aa87e065a3858e121bb
sorz <orz@sorz.org>
2016-05-02 20:10:21 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name="ssmanager",
+      version="0.1.0",
+      description="The python module interact with server-mode shadowsocks-libev.",
+      author="Shell Chen",
+      author_email="orz@sorz.org",
+      url="https://github.com/sorz/ssmanager/",
+      py_modules=['ssmanager'])
+
