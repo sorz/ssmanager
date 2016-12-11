779a98c942f21cef2347de6c163a0b76b40b74f3
sorz <orz@sorz.org>
2016-12-11 14:09:41 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="ssmanager",
-      version="0.2.3",
+      version="0.2.4",
       description="The python module interact with server-mode shadowsocks.",
       author="Shell Chen",
       author_email="orz@sorz.org",
