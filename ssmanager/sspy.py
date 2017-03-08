ea6753dc5add538fa21af322ae27a0eb6e01561d
sorz <orz@sorz.org>
2017-03-09 02:51:43 +1100

    Auto restart exited SS process

--- ssmanager/sspy.py
+++ ssmanager/sspy.py
@@ -3,13 +3,14 @@ import time
 import json
 import logging
 from subprocess import Popen, DEVNULL
-from threading import Thread, Event, Lock
+from threading import Thread, Event, RLock
 from socket import socket, AF_UNIX, SOCK_DGRAM
 
 from . import Server, _Manager
 
 
 SOCK_RESPONSE_TIMEOUT = 10
+MIN_RESTART_INTERVAL = 30
 
 class Manager(_Manager):
 
@@ -23,12 +24,39 @@ class Manager(_Manager):
         self._manager_addr = manager_addr
         self._client_addr = client_addr
         self._ok = Event()
-        self._sock_lock = Lock()
-
+        self._sock_lock = RLock()
         self._recv_thread = Thread(target=self._receiving, daemon=True)
+        self._restart_thread = Thread(target=self._restarting, daemon=True)
 
     def start(self):
         super().start()
+        self._start_process()
+        self._restart_thread.start()
+        logging.info('Manager started.')
+
+    def stop(self):
+        super().stop()
+        if self._sock is not None:
+            self._sock.close()
+        if self._ss_proc is not None:
+            self._ss_proc.terminate()
+        self._clean_socket()
+
+    def _clean_socket(self):
+        """Remove UNIX domain socket files if exists."""
+        if os.path.exists(self._manager_addr):
+            os.remove(self._manager_addr)
+        if os.path.exists(self._client_addr):
+            os.remove(self._client_addr)
+
+    def _send(self, message: str):
+        """Send a message to SS process and waiting for "ok"."""
+        with self._sock_lock:
+            self._sock.send(message.encode())
+            if not self._ok.wait(SOCK_RESPONSE_TIMEOUT):
+                raise SSServerConnectionTimeout()
+
+    def _start_process(self):
         if self._print_ss_log:
             output = None  # inherited from self
         else:
@@ -53,45 +81,46 @@ class Manager(_Manager):
             logging.critical('Cannot connect to ssserver process on %s.',
                              self._manager_addr)
             raise SSServerConnectionError()
-        self._recv_thread.start()
 
-        # Don't acquire sock lock here.
-        self._sock.send(b'remove: {"server": "127.0.1.2"}')
-        self._wait_ok()
-        logging.info('Manager started.')
-
-    def stop(self):
-        super().stop()
-        if self._sock is not None:
-            self._sock.close()
-        if self._ss_proc is not None:
-            self._ss_proc.terminate()
-        if os.path.exists(self._manager_addr):
-            os.remove(self._manager_addr)
-        if os.path.exists(self._client_addr):
-            os.remove(self._client_addr)
-
-    def _wait_ok(self):
-        """Waiting for SS process return "ok" via socket."""
-        if not self._ok.wait(SOCK_RESPONSE_TIMEOUT):
-            raise SSServerConnectionTimeout()
+        if not self._recv_thread.is_alive():
+            self._recv_thread = Thread(target=self._receiving, daemon=True)
+            self._recv_thread.start()
+        self._send('remove: {"server": "127.0.1.2"}')
+        logging.info('Shadowsocks process started.')
 
     def _start_instance(self, server):
         server.is_running = True
         config = server._config.copy()
         config['one_time_auth'] = config.pop('auth')
-        with self._sock_lock:
-            self._sock.send(b'add: ' + json.dumps(config).encode())
-            self._wait_ok()
+        self._send('add: ' + json.dumps(config))
         logging.debug('ss-server at %s:%d started.' % (server.host, server.port))
 
     def _stop_instance(self, server):
         server.is_running = False
-        with self._sock_lock:
-            self._sock.send(b'remove: {"server_port":' + str(server.port).encode() + b'}')
-            self._wait_ok()
+        self._send('remove: {"server_port": %d}' % server.port)
         logging.debug('ss-server at %s:%d stopped.' % (server.host, server.port))
 
+
+    def _restarting(self):
+        """A thread to watch SS process and try restart it when
+        process exited.
+        """
+        last_restart = time.time()
+        while self._is_running:
+            returncode = self._ss_proc.wait()
+            logging.warning('Shadowsocks process exited with code %s.', returncode)
+            with self._sock_lock:  # To block sock operations until process restarted.
+                restart_gap = time.time() - last_restart
+                if restart_gap < MIN_RESTART_INTERVAL:
+                    sleep = MIN_RESTART_INTERVAL - restart_gap
+                    logging.info('Waiting %d seconds before restart.', sleep)
+                    time.sleep(sleep)
+                logging.info('Restarting Shadowsocks process.')
+                last_restart = time.time()
+                self._clean_socket()
+                self._start_process()
+                for server in self._servers.values():
+                    self._start_instance(server)
 
     def _receiving(self):
         while self._is_running:
